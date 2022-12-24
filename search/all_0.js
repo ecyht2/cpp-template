3aad44534bdaf4f0fefc96c638845ca56b74fb11
ecyht2 <ecyht2@users.noreply.github.com>
2022-12-24 05:51:50 +0000

    deploy: 190eb1e827a2d5584f99dc47be0044e677fb3f3f

---
+++ search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hello_2dworld_0',['Hello-World',['../index.html',1,'']]]
+];
---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hello_2dworld_7',['Hello-World',['../index.html',1,'']]]
+];

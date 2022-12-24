dc4b9fce0cb3381de9d3db788603ffc2f235d145
ecyht2 <94816144+ecyht2@users.noreply.github.com>
2022-12-24 13:48:05 +0800

    Create main.cpp

---
+++ src/hello-world/main.cpp
@@ -0,0 +1,17 @@
+/** @file main.cpp Main file containing the main function.
+ *
+ * Copyright [2022] ecyht2
+ * */
+
+#include <iostream>
+
+/** The main function.
+ *
+ * @param argc The number of arguments passed in.
+ * @param argv The arguments passed in.
+ * @return The exit status.
+ * */
+int main(int argc, char *argv[]) {
+  std::cout << "Hello World" << std::endl;
+  return 0;
+}
